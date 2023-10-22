1f16b21175f9e087e1f37e2e9d610513e6ee1844
racemchoura <147626118+racemchoura@users.noreply.github.com>
2023-10-22 15:37:11 +0200

    Add files via upload

---
+++ TP2_SOA/src/Service/Banque_service.java
@@ -0,0 +1,10 @@
+package Service;
+import java.util.List;
+public class Banque_service {
+	public double conversion(double mt) {
+		return mt*10.6;
+		}
+	public List<Compte>getComptes(List<Compte> c) {
+		return c ;
+	}
+		}
---
+++ TP2_SOA/src/Service/Test.java
@@ -0,0 +1,14 @@
+package Service;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Banque_service bs = new Banque_service();
+		
+		System.out.println(bs.conversion(50));
+		
+
+	}
+
+}
